24e685f21943607bd84034ab866baa0c79f69b8e
rahimov19 <rahimov19.ar@gmail.com>
2023-03-28 15:14:05 +0200

    server

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -36,6 +36,25 @@ usersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
   }
 });
 
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UsersModel.checkCredentials(email, password);
+    if (user) {
+      const payload = {
+        _id: user._id,
+        terminalCode: user.terminalCode,
+      };
+      const accessToken = await createAccessToken(payload);
+      res.send({ user, accessToken });
+    } else {
+      next(createHttpError(401, "Credentials are not OK!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 usersRouter.post("/position", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const newPosition = new PositionModel(req.body);
@@ -160,25 +179,6 @@ usersRouter.get("/:userId", JWTAuthMiddleware, async (req, res, next) => {
       res.send(user);
     } else {
       next(createHttpError(404, `User with provided id not found`));
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-usersRouter.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await UsersModel.checkCredentials(email, password);
-    if (user) {
-      const payload = {
-        _id: user._id,
-        terminalCode: user.terminalCode,
-      };
-      const accessToken = await createAccessToken(payload);
-      res.send({ user, accessToken });
-    } else {
-      next(createHttpError(401, "Credentials are not OK!"));
     }
   } catch (error) {
     next(error);
